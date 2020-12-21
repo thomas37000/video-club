7827105f121f2afd0aa7af674be392cd89f83242
thomas <thomas.chalanson@sfr.fr>
2020-12-21 12:41:19 +0100

    put a title h1

---
+++ src/components/getAllMovies.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./getAllMovies.css";
+
+
+
+function getAllMovies(){
+  return (
+    <div>
+      {/* Bonjour je souhaoiterais utiliser votre Api, en effet je fais un projet
+      perso avec React. Créer un site de streaming tel Netflix. je suis étudiant
+      à la Wild Code School de Nantes et j' aimerais bien présenter ce projet
+      perso dans mon portfolio. Cordialement Thomas Chalanson
+      https://thomas-chalanson-portfolio.netlify.app/ */}
+      
+    </div>
+  );
+};
+
+getAllMovies.propTypes = {};
+
+export default getAllMovies;
